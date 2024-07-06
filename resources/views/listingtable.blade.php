a2e1d08496c73e4388d01d00abf7ed11b8b81f61
mobi12-tech <mrmobiii12@gmail.com>
2024-07-06 18:17:23 +0500

    testing for laravelui

---
+++ resources/views/listingtable.blade.php
@@ -0,0 +1,27 @@
+<div>
+    <!-- Happiness is not something readymade. It comes from your own actions. - Dalai Lama -->
+    <table class="table">
+  <thead class="dark">
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Name</th>
+      <th scope="col">Email</th>
+      <th scope="col">Message</th>
+    </tr>
+  </thead>
+  <tbody>
+  
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    
+  </tbody>
+</table>
+
+<table class="table">
+  
+</table>
+</div>
